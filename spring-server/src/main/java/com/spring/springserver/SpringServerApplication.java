39756cab4ec00682053d5336967e1ae59408b490
luolw <1185223710@qq.com>
2018-12-14 11:44:29 +0800

    初始化

---
+++ spring-server/src/main/java/com/spring/springserver/SpringServerApplication.java
@@ -0,0 +1,14 @@
+package com.spring.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerApplication.class, args);
+    }
+}
